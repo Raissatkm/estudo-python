00656cb974e3294784a00b6c6bb42de7c00307a2
Álefe Cruz <98561479+Raissatkm@users.noreply.github.com>
2024-04-21 21:36:57 -0300

    .

---
+++ num_linhas.py
@@ -0,0 +1,7 @@
+num_linhas = 9
+
+for i in range (1, num_linhas +1):
+    for j in range(1, i + 1):
+        print(i, end="")
+print("Fim!")
+        
---
+++ while1.py
@@ -0,0 +1,4 @@
+numero = 1 
+while numero != 0:
+    numero = int(input("Digte um numero:"))
+    print("você digitou zero!")
